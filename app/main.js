5e8a762cbd284114ed1339a4ea4a527a0ca263b6
Karla Kunze <karlalkunze@gmail.com>
2018-05-30 14:57:46 -0400

    lots

--- app/main.js
+++ app/main.js
@@ -1,12 +1,22 @@
+//input form
 let weatherButton = document.getElementById('weatherButton');
+let apiId = "d710a4aa5b152e9b5fe7739a6ccca894"
+let zipInput = "zipCode"
 
 //output variables
+
 let output = document.getElementById('output');
 let cityOutput = document.getElementById('cityOutput');
 let tempK = document.getElementById('temperatureOutputK');
 let tempF = document.getElementById('temperatureOutputF');
 let tempC = document.getElementById('temperatureOutputC');
 let condition = document.getElementById('condition');
+
+//error variables
+
+let error = document.getElementById("error");
+
+
 
 let results = {
   "coord": {
@@ -61,7 +71,7 @@ document.onreadystatechange = function() {
 
   weatherButton.onclick = getWeather;
 
-  console.log(results);
+  // console.log(results);
   //console.log(results.name);
   //console.log(results.main.temp);
   //console.log(results.weather[0].description);
@@ -70,19 +80,58 @@ document.onreadystatechange = function() {
 
 //if results.cod = 200
 
+var url = "https://api.openweathermap.org/data/2.5/weather?zip=<zipCode>,us&appid=<appId>"
+url = url.replace("<zipCode>", zipInput.value);
+url = url.replace("<appId>", appId);
+
+
 function getWeather() {
-  //alert("test function");
-  cityOutput.innerHTML = results.name;
-  tempK.innerHTML = Math.round(results.main.temp) + " K";
-  tempF.innerHTML = convertKtoF(results.main.temp) + "&degF";
-  tempC.innerHTML = convertKtoC(results.main.temp) + "&degC";
-  condition.innerHTML = results.weather[0].description;
+//code that fetches API data and stores it in resuls
+
+apiRequest = new XMLHttpREquest();
+apiRequest.onload = catchResponse;
+apiRequest.onerror = httpRequestOnError;
+apiRequest.open('get', url, true);
+apiRequest.send();
+
+
+
+
+
 
   output.style.display = "block";
 }
 //else if results.cod = 401
 
 //elseif results.cod = 404
+
+funtion catchRequestOnError() {
+  //to do
+  errorMessage.innerHTML = "There was a problem accessing the API. Try again Later"
+  error.style.display = 'block';
+
+}
+
+
+function catchResponse() {
+  // to do
+  if (apiRequest.statusText === "OK"){
+
+    let response = JSON.parse(apiRequest.responseText);
+
+    error.style.display = "none";
+    cityOutput.innerHTML = response.name;
+    tempK.innerHTML = Math.round(response.main.temp) + " K";
+    tempF.innerHTML = convertKtoF(response.main.temp) + "&degF";
+    tempC.innerHTML = convertKtoC(response.main.temp) + "&degC";
+    condition.innerHTML = response.weather[0].description;
+
+  }
+    else {
+        error.style.display = 'block';
+        errorMessage.innerHTML = apiRequest.statusText;
+    }
+  }
 
 
 function convertKtoF(kelvin) {
@@ -95,47 +144,17 @@ function convertKtoC(kelvin) {
   return Math.round(celsius);
 }
 
-
-
-//if zip is good, output:
-// 20180529174915
-// https://api.openweathermap.org/data/2.5/weather?zip=40504,us&appid=d710a4aa5b152e9b5fe7739a6ccca894
-
-//if zip is bad
-// cod 404
-
-//if api bad
-//cod 401
-
-// {
-//   "coord": {
-//     "lon": -84.54,
-//     "lat": 38.04
-//   },
-//   "weather": [
-//     {
-//       "id": 803,
-//       "main": "Clouds",
-//       "description": "broken clouds",
-//       "icon": "04d"
-//     }
-//   ],
-//   "base": "stations",
-//   "main": {
-//     "temp": 301.5,
-//     "pressure": 1011,
-//     "humidity": 65,
-//     "temp_min": 301.15,
-//     "temp_max": 302.15
-//   },
-//   "visibility": 16093,
-//   "wind": {
-//     "speed": 5.7,
-//     "deg": 90,
-//     "gust": 10.3
-//   },
-//   "clouds": {
-//     "all": 75
-//   },
-//   "dt": 1527628500,
-//   "sys": {
+function displayImage(tempF) {
+  if (tempF > 85) {
+    weatherImage.src = "https:/goo.gl/c8VxVr";
+  }
+  else if (tempF > 65) {
+    weatherImage.src = "https:/goo.gl/WNV85G";
+  }
+  else if (tempF > 32) {
+    weatherImage.src = "https:/goo.gl/KAbVwR";
+  }
+  else {
+    weatherImage.src = "https:/goo.gl/KAbVwR";
+  }
+}
